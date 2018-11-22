c7092b3f7a716bfe252b5a5f88deb37f6f93ddb5
mark <m.vermulst@student.fontys.nl>
2018-11-22 15:08:36 +0100

    Begin Google maps

---
+++ OutsourceGoogleMaps/OutsourceGoogleMaps/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OutsourceGoogleMaps
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
